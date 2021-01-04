443e7f30a046b0857c50dbc554fec1432a5cf1b4
Ban Nguyen <zeddy502@gmail.com>
2021-01-04 08:06:34 +0700

    Update sails.io.ts

--- src/sails.io.ts
+++ src/sails.io.ts
@@ -9,7 +9,4 @@ if (window.io) {
   window.io.sails.reconnection = true;
   window.io.sails.environment = env.NODE_ENV === 'staging' ? 'development' : env.NODE_ENV;
   Vue.prototype.$io = window.io;
-  window.io.socket.on('message', function(data: any) {
-    console.log('On message data', data);
-  });
 }
